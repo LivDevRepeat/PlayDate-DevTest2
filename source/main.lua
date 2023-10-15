d3df4964b81d41b018bdb59a0c82cc825ca6544c
LivReTaRDIS <lv.zuhause@gmail.com>
2023-10-15 19:37:31 +0200

    Testing and moving hello world

--- source/main.lua
+++ source/main.lua
@@ -1,8 +1,10 @@
 import "CoreLibs/graphics"
+import "CoreLibs/object"
+import "CoreLibs/sprites"
+import  "CoreLibs/timer"
 
 local gfx <const> = playdate.graphics
-
 function playdate.update() 
     gfx.clear()
-    gfx.drawText("Hello World", 20, 20)
+    gfx.drawText("Hello World", 89, 20)
 end

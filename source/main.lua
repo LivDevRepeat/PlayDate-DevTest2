2581fa878ed0457287b3b475fb2162a7a48f46fe
LivReTaRDIS <lv.zuhause@gmail.com>
2023-11-11 14:08:24 +0100

    Tried first for loop

--- source/main.lua
+++ source/main.lua
@@ -6,15 +6,23 @@ import  "CoreLibs/timer"
 import "Test"
 
 local gfx <const> = playdate.graphics
-local i = 0;
-function playdate.update() 
-    gfx.clear()
-    i+=1
-    test(i)
+local inrData = {}
+local i = 0
+function playdate.update()  
+    if playdate.buttonJustReleased( playdate.kButtonA) then
+       test(i)
+       i+=1
+    end
 end
 
 function test(i)
-    text = "test Function got called! FRAME: " .. tostring(i)
-    gfx.drawText(text, 20, 20)
+    gfx.clear()
+    inrData[i]= "test Function got called! FRAME: " .. tostring(i)
+
+    for  j=0,i,1 do
+     
+        gfx.drawText(inrData[j], 20, 20*j)
+        end
+  
     playdate.datastore.write(table, "test",true)
 end

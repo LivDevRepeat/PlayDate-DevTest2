bdc2525c206fd10e325c934d4c62c6489c9dd902
LivReTaRDIS <lv.zuhause@gmail.com>
2023-11-11 17:04:45 +0100

    Save Data willl be wrtitten with keyboart

--- source/main.lua
+++ source/main.lua
@@ -2,6 +2,7 @@ import "CoreLibs/graphics"
 import "CoreLibs/object"
 import "CoreLibs/sprites"
 import  "CoreLibs/timer"
+import "CoreLibs/keyboard"
 
 import "Test"
 
@@ -12,22 +13,36 @@ local j = 0
 local inrData = {}
 
 function debugReadData()
-  local inrSaveData = playdate.datastore.read()
-  inrData = inrSaveData
+      local inrSaveData = playdate.datastore.read()
+      inrData = inrSaveData
+      
+      if inrData == nil then
+          inrData = {}
+      end
  end
 
 debugReadData()
 
 function playdate.update()  
-    if playdate.buttonJustReleased( playdate.kButtonA) then
-        addAndDrawNewTableEntry()
-        j+=1
-    end
+    gfx.clear();
     
-    if playdate.buttonJustPressed(playdate.kButtonB) then
-        j=0
-        gfx.clear()
+    if playdate.keyboard.isVisible() then 
+        debugKeyBoardControls()
+        gfx.drawText("Next ENTRY will be: " .. playdate.keyboard.text  , 20, 100)
+    else
+        debugControlls()   
+    end 
+    gfx.drawText("Welcome to the debug Save Data Experiment", 20, 20)
+    if #inrData == 0 then
+        gfx.drawText("NO INR", 20, 40)
+    else
+        gfx.drawTextInRect("The INRdata Currently Saves ".. #inrData .. " Entries", 20, 50, 400, 80)
+        gfx.drawText("Last Entry: "  .. inrData[#inrData], 20, 70)
     end
+
+end
+
+function debugControlls()
     
     if playdate.buttonJustPressed(playdate.kButtonUp) then
         playdate.datastore.write(inrData)
@@ -41,12 +56,22 @@ function playdate.update()
         playdate.datastore.delete()
     end
     
+    if playdate.buttonJustPressed(playdate.kButtonRight) then
+        playdate.keyboard.show()   
+    end
 end
 
-function addAndDrawNewTableEntry()
-    i = #inrData +1;
-    inrData[i] = "I created " .. tostring(i) .. " entries in the inr save data!"
-    gfx.drawText(inrData[i], 0, 20*j)
+function debugKeyBoardControls()
+    
+    if playdate.buttonJustPressed(playdate.kButtonB) then
+    playdate.keyboard.hide()
+    end 
+    
+end
+
+function playdate.keyboard.keyboardWillHideCallback() 
+     i = #inrData + 1
+     inrData[i] = playdate.keyboard.text  
 end
 
 

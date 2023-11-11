d98cabd8705a252d3b85b287df9736baf1d677a6
LivReTaRDIS <lv.zuhause@gmail.com>
2023-11-11 13:32:59 +0100

    Add Test LUA Learnings

---
+++ source/Test.lua
@@ -0,0 +1,7 @@
+name = "Default Name"
+height = "1.68"
+
+function init(name,heightarg, ...)
+    this.name = name
+	this.height = height
+end
--- source/main.lua
+++ source/main.lua
@@ -3,8 +3,18 @@ import "CoreLibs/object"
 import "CoreLibs/sprites"
 import  "CoreLibs/timer"
 
+import "Test"
+
 local gfx <const> = playdate.graphics
+local i = 0;
 function playdate.update() 
     gfx.clear()
-    gfx.drawText("Hello World", 89, 20)
+    i+=1
+    test(i)
+end
+
+function test(i)
+    text = "test Function got called! FRAME: " .. tostring(i)
+    gfx.drawText(text, 20, 20)
+    playdate.datastore.write(table, "test",true)
 end
